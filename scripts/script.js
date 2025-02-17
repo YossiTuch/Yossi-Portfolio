84e9719224fc5fcbddc1dc4bdc47a8fcd5cea454
Yossi <yossi6435@gmail.com>
2025-02-17 19:25:39 +0200

    Added validity check to the contact section and changed the message input to textarea element to make it look more like a message input.

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
+import { formValidityCheck } from './validity.js';
+
 const contactForm = document.getElementById('contact-form');
-const nameInput = document.getElementById('name-input');
-const emailInput = document.getElementById('email-input');
-const phoneInput = document.getElementById('phone-input');
-const messageInput = document.getElementById('message-input');
+// const nameInput = document.getElementById('name-input');
+// const emailInput = document.getElementById('email-input');
+// const phoneInput = document.getElementById('phone-input');
+// const messageInput = document.getElementById('message-input');
 
 const cvButton = document.getElementById('cv-button');
 
-contactForm.addEventListener('click', e => {
+contactForm.addEventListener('submit', e => {
   e.preventDefault();
-});
 
-//TODO: Send elements textContent to the eventListener
+  formValidityCheck();
+});
 
 //CV Button alert placeholder
 cvButton.addEventListener('click', () => {
--- scripts/validity.js
+++ scripts/validity.js
@@ -1,3 +1,60 @@
-const formValidityCheck = (name, email, phone, message) => {};
+const isEmail = email => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(String(email).toLowerCase());
+};
 
-//TODO: make validation check function for each parameter and the send the and make function call for each - export only the main function
+export const formValidityCheck = () => {
+  const nameInput = document.getElementById('name-input');
+  const emailInput = document.getElementById('email-input');
+  const phoneInput = document.getElementById('phone-input');
+  const messageInput = document.getElementById('message-input');
+
+  nameInput.classList.remove('invalid');
+  emailInput.classList.remove('invalid');
+  phoneInput.classList.remove('invalid');
+  messageInput.classList.remove('invalid');
+
+  const nameValue = nameInput.value.trim();
+  const emailValue = emailInput.value.trim();
+  const phoneValue = phoneInput.value.trim();
+  const messageValue = messageInput.value;
+
+  if (nameValue === '') {
+    nameInput.classList.add('invalid');
+    alert('Name Required');
+    return;
+  } else if (nameValue.length <= 1) {
+    nameInput.classList.add('invalid');
+    alert('Name should contain more than 1 character');
+    return;
+  }
+
+  if (!isEmail(emailValue)) {
+    emailInput.classList.add('invalid');
+    alert('Email is Invalid');
+    return;
+  }
+
+  if (phoneValue.length !== 10) {
+    phoneInput.classList.add('invalid');
+    alert('Invalid phone number');
+    return;
+  }
+  if (messageValue === '') {
+    messageInput.classList.add('invalid');
+    alert('Message required');
+    return;
+  }
+
+  nameInput.classList.remove('invalid');
+  emailInput.classList.remove('invalid');
+  phoneInput.classList.remove('invalid');
+  messageInput.classList.remove('invalid');
+
+  nameInput.value = '';
+  emailInput.value = '';
+  phoneInput.value = '';
+  messageInput.value = '';
+
+  alert('Message Sent successfully');
+};
