767cbd58fec8505ae2bff168d3a7dcb4dfaf3c55
Yossi <yossi6435@gmail.com>
2025-02-17 19:29:36 +0200

    Removed a comment

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,6 @@
 import { formValidityCheck } from './validity.js';
 
 const contactForm = document.getElementById('contact-form');
-// const nameInput = document.getElementById('name-input');
-// const emailInput = document.getElementById('email-input');
-// const phoneInput = document.getElementById('phone-input');
-// const messageInput = document.getElementById('message-input');
 
 const cvButton = document.getElementById('cv-button');
 
